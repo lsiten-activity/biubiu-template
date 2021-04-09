0456c796776ecfeaac5b499519bd49f5b7572a0b
雷诗城 <leishicheng@MacBook-Pro-3.local>
2021-04-09 16:17:30 +0800

    template

---
+++ biubiu.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'biubiu-template',
+  templateName: '营销活动',
+  author: 'muwoo',
+  snapshot: 'https://cdn.img.snapshot.png',
+  gitUrl: 'lsiten-activity/biubiu-template',
+  projectType: '0'
+};
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <BiubiuComponent>
+      <biubiu-banner :obj="{
+        src: 'https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/a2e81c9d78a24c5fb0cb33bfcc366dce~tplv-k3u1fbpfcp-watermark.image',
+        link: 'https://www.baidu.com',
+      }" />
+      <biubiu-form />
+    </BiubiuComponent>
+  </div>
+</template>
+
+<script>
+import BiubiuComponent from './common/biubiu-component';
+import BiubiuBanner from './components/biubiu-banner/index';
+import BiubiuForm from './components/biubiu-form/index';
+export default {
+  name: 'App',
+  components: {
+    BiubiuBanner,
+    BiubiuForm,
+    BiubiuComponent,
+  }
+}
+</script>
